53e948ec82aec3905c0725cac7e33950fcd8b297
KhairulIzwan <wansnap@gmail.com>
2020-07-22 17:13:31 +0800

    Updated on 07-22-2020-12:43:17

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=['dl4cv_pyimagesearch_utils_captchahelpers'], package_dir={'': 'src'})
+d = generate_distutils_setup(packages=['dl4cv_pyimagesearch_utils_captchahelper'], package_dir={'': 'src'})
 
 setup(**d)
